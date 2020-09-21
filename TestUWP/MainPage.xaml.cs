b4bad7764219c51f6662812e762846739c3adcc9
Tomer Lavi <tomer265@hotmail.com>
2020-09-22 02:46:38 +0300

    Initial Commit

--- TestUWP/MainPage.xaml.cs
+++ TestUWP/MainPage.xaml.cs
@@ -18,6 +18,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Streams;
+using System.Text;
+using TestUWP.Models;
+using System.Xml.Linq;
+using System.Xml;
+using MongoDB.Driver;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,54 +34,104 @@ namespace TestUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SmartCardReader reader;
         public MainPage()
         {
             this.InitializeComponent();
+            Task setReaderTask = SetReaderAndEvents();
+            Task.Run(async () => await setReaderTask);
+        }
+
+        private async Task SetReaderAndEvents()
+        {
+            string selector = SmartCardReader.GetDeviceSelector();
+            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
+
+            DeviceInformation device = devices[0];
+            reader = await SmartCardReader.FromIdAsync(device.Id);
+            reader.CardAdded += Reader_CardAdded;
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             await CardAuth();
-            //Task.Run(async () => await CardAuth());
         }
 
         public async Task CardAuth()
         {
-            Guid cardGuid = Guid.Empty;
-            tbOutput.Text += "Entered\n";
             try
             {
-                string selector = SmartCardReader.GetDeviceSelector();
-                DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
-
-                DeviceInformation device = devices[0];
-                SmartCardReader reader = await SmartCardReader.FromIdAsync(device.Id);
                 IReadOnlyList<SmartCard> cards = await reader.FindAllCardsAsync();
-                SmartCard smartCard = cards.First();
+                if (cards.Any())
+                {
+                    SmartCard smartCard = cards.First();
 
-                IAsyncOperation<SmartCardProvisioning> provisioning = SmartCardProvisioning.FromSmartCardAsync(smartCard);
+                    string buffer64BaseString = await GetCardBase64String(smartCard);
+                    CardHolder cardHolderFromDb = GetCardHolderFromDB(@buffer64BaseString);
 
-                var task = await Task.Run(() => provisioning.GetResults().GetIdAsync());
-                var results = task.GetResults();
-                
-                
-                //cardGuid = t.res;
-
-                tbOutput.Text += cardGuid + Environment.NewLine;
+                    if (cardHolderFromDb != null)
+                    {
+                        tbOutput.Text += "Name: " + cardHolderFromDb.FirstName + cardHolderFromDb.LastName + Environment.NewLine;
+                        tbOutput.Text += "Date Of Birth" + cardHolderFromDb.DateOfBirth.ToString("dd/MM/yyyy") + Environment.NewLine;
+                    }
+                    else
+                    {
+                        tbOutput.Text += "No user detected for this card." + Environment.NewLine;
+                    }
+                }
+                else
+                {
+                    tbOutput.Text += "No cards detected." + Environment.NewLine;
+                }
             }
             catch (Exception e)
             {
+                tbOutput.Text += "Error reading the card." + Environment.NewLine + Environment.NewLine;
                 tbOutput.Text += e.Message + Environment.NewLine;
                 tbOutput.Text += e.InnerException + Environment.NewLine;
                 tbOutput.Text += e.StackTrace + Environment.NewLine;
                 tbOutput.Text += e.Source + Environment.NewLine;
-                tbOutput.Text += e.Data + Environment.NewLine;
+                tbOutput.Text += e.Data + Environment.NewLine + Environment.NewLine + Environment.NewLine;
             }
         }
 
-        private void Reader_CardAdded(SmartCardReader sender, CardAddedEventArgs args)
+        private async void Reader_CardAdded(object sender, CardAddedEventArgs args)
         {
-            var a = 1;
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                await CardAuth();
+            });
+        }
+
+        private async Task<string> GetCardBase64String(SmartCard smartCard)
+        {
+            var buffer = await smartCard.GetAnswerToResetAsync();
+            DataReader bufferReader = DataReader.FromBuffer(buffer);
+            byte[] fileContent = new byte[bufferReader.UnconsumedBufferLength];
+            bufferReader.ReadBytes(fileContent);
+            string buffer64BaseString = Convert.ToBase64String(fileContent);
+
+            return buffer64BaseString;
+        }
+
+        private CardHolder GetCardHolderFromDB(string CardIdentifier)
+        {
+            string connectionString = GetConnectionStringFromConfigFile().Trim();
+            MongoClient client = new MongoClient(@connectionString);
+            IMongoDatabase dataBase = client.GetDatabase("CardReaderUWP");
+            IMongoCollection<CardHolder> collection = dataBase.GetCollection<CardHolder>("CardHolders");
+
+            CardHolder result = collection.Find(ch => ch.CardIdentifier == CardIdentifier).FirstOrDefault();
+
+            return result;
+        }
+
+        private string GetConnectionStringFromConfigFile()
+        {
+            XmlDocument configurationsFile = new XmlDocument();
+            configurationsFile.Load("Configurations.xml");
+            XmlNode node = configurationsFile.SelectSingleNode("/AppSettings/ConnectionString");
+            return node.InnerText;
         }
     }
 }
---
+++ TestUWP/Models/CardHolder.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUWP.Models
+{
+    public class CardHolder
+    {
+        public ObjectId Id { get; set; } // MongoDb objectId
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PicUrl { get; set; }
+        public string VocalFileUrl { get; set; }
+        public string CardIdentifier { get; set; }
+    }
+}
